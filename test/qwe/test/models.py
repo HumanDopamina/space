95aad56e0d505d6ee57fd4ff8ae86d7ec9514580
HumanDopamina <leonelher0307@gmail.com>
2026-09-05 15:13:53 -0600

    Inicianlizando carpeta

---
+++ test/qwe/test/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=200)
+    birthday = models.DateField(null=True, blank=True)  
+    country = models.CharField(max_length=200, blank=True)
+
+    def __str__(self):
+        return self.name
+
